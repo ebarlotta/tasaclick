0139cd175109c8dca5416441190e46f428fa7b1f
Gerardo <86392326+teresa2808@users.noreply.github.com>
2022-09-03 14:46:28 -0300

    frente y fondo

--- database/seeders/CoeficientesSeeder.php
+++ database/seeders/CoeficientesSeeder.php
@@ -14,15 +14,173 @@ class CoeficientesSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'30','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'32','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'34','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'36','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'38','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'30','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'30','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'30','coeficiente'=>'0.85']);
-        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'30','coeficiente'=>'0.85']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'10','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'10','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'10','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'10','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'10','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'10','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'10','coeficiente'=>'1.50']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'10','coeficiente'=>'1.50']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'10','coeficiente'=>'1.45']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'10','coeficiente'=>'1.45']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'10','coeficiente'=>'1.40']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'10','coeficiente'=>'1.40']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'10','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'10','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'10','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'10','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'10','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'10','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'10','coeficiente'=>'1.20']);
+        
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'12','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'12','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'12','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'12','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'12','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'12','coeficiente'=>'1.55']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'12','coeficiente'=>'1.50']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'12','coeficiente'=>'1.50']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'12','coeficiente'=>'1.45']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'12','coeficiente'=>'1.45']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'12','coeficiente'=>'1.40']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'12','coeficiente'=>'1.40']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'12','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'12','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'12','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'12','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'12','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'12','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'12','coeficiente'=>'1.20']);
+
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'14','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'14','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'14','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'14','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'14','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'14','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'14','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'14','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'14','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'14','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'14','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'14','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'14','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'14','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'14','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'14','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'14','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'14','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'14','coeficiente'=>'1.10']);
+
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'16','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'16','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'16','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'16','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'16','coeficiente'=>'1.35']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'16','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'16','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'16','coeficiente'=>'1.30']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'16','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'16','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'16','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'16','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'16','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'16','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'16','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'16','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'16','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'16','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'16','coeficiente'=>'1.10']);
+
+
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'18','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'18','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'18','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'18','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'18','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'18','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'18','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'18','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'18','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'18','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'18','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'18','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'18','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'18','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'18','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'18','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'18','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'18','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'18','coeficiente'=>'1.00']);
+
+
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'20','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'20','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'20','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'20','coeficiente'=>'1.25']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'20','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'20','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'20','coeficiente'=>'1.20']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'20','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'20','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'20','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'20','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'20','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'20','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'20','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'20','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'20','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'20','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'20','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'20','coeficiente'=>'1.00']);
+
+
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'22','coeficiente'=>'0.90']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'22','coeficiente'=>'0.90']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'22','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'22','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'22','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'22','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'22','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'22','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'22','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'22','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'22','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'22','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'22','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'22','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'22','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'22','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'22','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'22','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'22','coeficiente'=>'0.95']);
+
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'6','fila'=>'24','coeficiente'=>'0.90']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'8','fila'=>'24','coeficiente'=>'0.90']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'10','fila'=>'24','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'12','fila'=>'24','coeficiente'=>'1.15']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'14','fila'=>'24','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'16','fila'=>'24','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'18','fila'=>'24','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'20','fila'=>'24','coeficiente'=>'1.10']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'22','fila'=>'24','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'24','fila'=>'24','coeficiente'=>'1.05']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'26','fila'=>'24','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'28','fila'=>'24','coeficiente'=>'1.00']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'30','fila'=>'24','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'32','fila'=>'24','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'34','fila'=>'24','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'36','fila'=>'24','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'38','fila'=>'24','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'40','fila'=>'24','coeficiente'=>'0.95']);
+        // DB::table('coeficientesfrentefondo')->insert(['columna'=>'42','fila'=>'24','coeficiente'=>'0.95']);
+
+
+
+
+
 
         DB::table('coeficientes')->insert(['tabladecoeficiente'=>'CoeficienteDeEsquina','descripcion'=>'-','coeficiente'=>'1']); //Ninguno
         DB::table('coeficientes')->insert(['tabladecoeficiente'=>'CoeficienteDeEsquina','descripcion'=>'Zona comercial','coeficiente'=>'1.2']);
